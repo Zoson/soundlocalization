ef845b39ab9b034f0d0326f092dfc60273014e23
zoson <597103316@qq.com>
2016-07-12 14:00:29 +0800

    zoson

--- project/module/net/server.cpp
+++ project/module/net/server.cpp
@@ -10,7 +10,7 @@
 
 Server::Server()
 {
-	this->m_post = 4999;
+	this->m_post = 5000;
 	this->m_client_num = 10;
 	this->m_listenfd = -1;
 	this->m_send = NULL;
@@ -129,6 +129,8 @@ void Server::startServer()
 						close(i);
 						continue;
 					}
+					char msg[] = "abc\n";
+					send(i,msg,sizeof(msg),0);
 				}
 			}
 		}
--- project/module/net/tests.cpp
+++ project/module/net/tests.cpp
@@ -1,24 +1,9 @@
 #include "net/server.h"
-#include "json/json.h"
 #include <string.h>
-#include "entity/entity.h"
 using namespace std;
 int main()
 {
-	//Server *server = new Server();
-	//server->startServer();
-	//字符串
-    const char* str = "{\"flag\":0,\"data\":\"start\"}";
-
-    Json::Reader reader;
-    Json::Value root;
-
-    //从字符串中读取数据
-    if(reader.parse(str,root))
-    {
-        
-        
-    }
-
+	Server *server = new Server();
+	server->startServer();
     return 0;
 }
